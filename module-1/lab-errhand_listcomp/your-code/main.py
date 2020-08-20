bc9fe134a4763af8807da5c9a3e8e325c05ccbe8
Ana Murillo <ana_murillo_amores@hotmail.com>
2020-08-20 18:16:50 +0200

    first commit error handling just in case

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -1,25 +1,28 @@
 #Example: 
 
-eggs = (1,3,8,3,2)
+'''eggs = (1,3,8,3,2)
 
 my_listComprehension = [1/egg for egg in eggs]
 
 print(my_listComprehension)
 
-#Insert here the module/library import statements 
+'''#Insert here the module/library import statements 
 
+import math
+import os, sys
 
-
-
-#1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
+'''#1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
+square = [i **2 for i in range(20)]
 
-
+print(square)
 
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
+power_of_two = [pow(2, 50)]
 
+print(power_of_two)
 
 
 
@@ -27,24 +30,31 @@ print(my_listComprehension)
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
 
+sqrt = [math.sqrt(i) for i in range(100)]
+print(sqrt)
 
 
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
+my_list = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]
 
+print(my_list)
 
 
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
 
+odds = [i for i in range( 1,100) if i % 2 != 0 ]
 
+print(odds)
 
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
-
+divisible_by_seven = [i for i in range( 1,1000) if i % 7 == 0 ]
+print(divisible_by_seven)
 
 
 
@@ -54,20 +64,27 @@ print(my_listComprehension)
 
 teststring = 'Find all of the words in a string that are monosyllabic'
 
-
-
+non_vowels = ["".join([i for i in teststring if i not in "aeiouAEIOU"])]
+print(non_vowels)
 
 #8. Find the capital letters (and not white space) in the sentence 'The Quick Brown Fox Jumped Over The Lazy Dog'. 
 # Use capital_letters as the name of the list.  
 # Remember to use list comprehensions and to print your results
 
+sentence = 'The Quick Brown Fox Jumped Over The Lazy Dog'
+
+capital_letters = [i for i in sentence if i >= "A" and i <= "Z"]
+
+print(capital_letters)'''
 
 
-
-#9. Find all the consonants in the sentence 'The quick brown fox jumped over the lazy dog'.
+'''#9. Find all the consonants in the sentence 'The quick brown fox jumped over the lazy dog'.
 # Use consonants as the name of the list.
 # Remember to use list comprehensions and to print your results.
 
+sentence = 'The quick brown fox jumped over the lazy dog'
+consonants = ["".join([i for i in sentence if i not in "aeiouAEIOU"])]
+print(consonants)
 
 
 
@@ -76,16 +93,21 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 # You will probably need to import os library and some of its modules. You will need to make some online research.
 # Remember to use list comprehensions and to print your results.
 
+files = [f for f in os.listdir("/Users/ana/datamad0820")]
+
+print(files)
+
 
 
 #11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
 
+import random
+random_lists = [random.sample(range(0,100),10) for i in list(range(4))]
+print(random_lists)
 
-
-
-#12. Flatten the following list of lists. Use flatten_list as the name of the output.
+'''#12. Flatten the following list of lists. Use flatten_list as the name of the output.
 # Remember to use list comprehensions and to print your results
 
 list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
@@ -186,6 +208,6 @@ def linux_interaction():
 # Hint: Create a class derived from the pre-defined Exception class in Python
 
 Total_Marks = int(input("Enter Total Marks Scored: ")) 
-Num_of_Sections = int(input("Enter Num of Sections: "))
+Num_of_Sections = int(input("Enter Num of Sections: "))'''
 
 
